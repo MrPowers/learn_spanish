803b8a899d3ce370ca8540b3d10c0013de06e765
Matthew Powers <matthewkevinpowers@gmail.com>
2015-06-28 19:36:11 -0400

    Add some additional verbs and fill in questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,41 @@ verbs = [
     frequency_rank: 5,
     spanish: "poder",
     english: "to be able, can"
+  },
+  {
+    frequency_rank: 6,
+    spanish: "decir",
+    english: "to say, to tell"
+  },
+  {
+    frequency_rank: 7,
+    spanish: "ir",
+    english: "to go"
+  },
+  {
+    frequency_rank: 8,
+    spanish: "ver",
+    english: "to see"
+  },
+  {
+    frequency_rank: 9,
+    spanish: "dar",
+    english: "to give"
+  },
+  {
+    frequency_rank: 10,
+    spanish: "saber",
+    english: "to know"
+  },
+  {
+    frequency_rank: 11,
+    spanish: "querer",
+    english: "to want, to love"
+  },
+  {
+    frequency_rank: 12,
+    spanish: "llegar",
+    english: "to arrive, to come, to reach"
   }
 ]
 
@@ -38,6 +73,55 @@ questions = [
     tense: "present",
     spanish_sentence: "Yo _____ ir a la playa",
     english_sentence: "I want to go to the beach"
+  },
+  {
+    pronoun: "yo",
+    verb: "ser",
+    tense: "present",
+    spanish_sentence: "Yo _____ alto",
+    english_sentence: "I am tall"
+  },
+  {
+    pronoun: "yo",
+    verb: "estar",
+    tense: "present",
+    spanish_sentence: "Yo _____ feliz",
+    english_sentence: "I am happy"
+  },
+  {
+    pronoun: "yo",
+    verb: "tener",
+    tense: "present",
+    spanish_sentence: "Yo _____ una pregunta",
+    english_sentence: "I have a question"
+  },
+  {
+    pronoun: "yo",
+    verb: "hacer",
+    tense: "present",
+    spanish_sentence: "Yo _____ la cama",
+    english_sentence: "I make the bed"
+  },
+  {
+    pronoun: "yo",
+    verb: "poder",
+    tense: "present",
+    spanish_sentence: "Yo _____ hablar con él",
+    english_sentence: "I can talk with him"
+  },
+  {
+    pronoun: "yo",
+    verb: "decir",
+    tense: "present",
+    spanish_sentence: "Yo _____ cosas estupidas a veces",
+    english_sentence: "I say stupid things sometimes"
+  },
+  {
+    pronoun: "yo",
+    verb: "ir",
+    tense: "present",
+    spanish_sentence: "Yo _____ a la tienda",
+    english_sentence: "I go to the store"
   }
 ]
 
