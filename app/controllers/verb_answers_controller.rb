3ea1b6f01b412332700f9967802e4286b74cf3fb
Matthew Powers <matthewkevinpowers@gmail.com>
2015-07-03 12:43:04 -0400

    Clean up the code that update the skill level after an answer

--- app/controllers/verb_answers_controller.rb
+++ app/controllers/verb_answers_controller.rb
@@ -2,25 +2,25 @@ class VerbAnswersController < ApplicationController
 
   def create
     @answer = VerbAnswer.create!(answer_params)
-    update_level(@answer.verb_question)
-    redirect_to verb_question_answer_path(answer_params[:verb_question_id])
+    update_level
+    redirect_to verb_question_answer_path(@answer.verb_question)
   end
 
   private
 
-  def update_level(verb_question)
-    # this code is wrong because we don't want the level to go above the maximum level
+  def update_level
     exam = VerbExam.where(user_id: current_user.id).first_or_create
-    last_answer_correct = (answer_params[:answer] == verb_question.correct_answer)
-    if last_answer_correct
+    if was_last_answer_correct?
       exam.skill_level += 1
     else
-      if exam.skill_level > 1 # don't want the skill_level to go below 1
-        exam.skill_level -= 1
-      end
+      exam.skill_level -= 1
     end
-    exam.was_last_answer_correct = last_answer_correct
+    exam.was_last_answer_correct = was_last_answer_correct?
     exam.save
+  end
+
+  def was_last_answer_correct?
+    @answer.answer == @answer.verb_question.correct_answer
   end
 
   def answer_params
