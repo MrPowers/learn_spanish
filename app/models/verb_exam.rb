d311bf88895a8c65e2c05ac8b3c7dabfdc7dd8bf
Matthew Powers <matthewkevinpowers@gmail.com>
2015-07-05 12:30:55 -0400

    Fix the algorithm to determine the next question and update the footer

--- app/models/verb_exam.rb
+++ app/models/verb_exam.rb
@@ -1,7 +1,7 @@
 class VerbExam < ActiveRecord::Base
 
   def percent_complete
-    r = ((skill_level - 1).to_f / total_question_count * 100).round(0)
+    r = ((skill_level - 1).to_f / total_verb_count * 100).round(0)
     return 100 if r > 100
     r
   end
@@ -14,12 +14,12 @@ class VerbExam < ActiveRecord::Base
 
   def next_question_frequency
     return 1 if skill_level <= 1
-    return rand(1..total_question_count) if skill_level >= total_question_count
+    return rand(1..total_verb_count) if skill_level >= total_verb_count
     was_last_answer_correct ? skill_level : rand(1..skill_level)
   end
 
-  def total_question_count
-    VerbQuestion.count
+  def total_verb_count
+    Verb.count
   end
 
 end
