6ddc887cb55bda1eb3c3b3df1dca1030d7b3060f
MrPowers <matthewkevinpowers@gmail.com>
2015-08-08 14:13:33 -0500

    Add tense validation to the Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,16 @@
 class Question < ActiveRecord::Base
   belongs_to :word
 
+  def self.tenses
+    [
+      "present",
+      "preterite",
+      "imperfect"
+    ]
+  end
+
+  validates :tense, inclusion: { in: tenses }, allow_blank: true
+
   validate :spanish_contains_five_underscores
 
   def spanish_contains_five_underscores
