8ee0e28bea588e8bcf4766b79fbe1e28af3b9ed8
MrPowers <matthewkevinpowers@gmail.com>
2015-08-08 12:09:59 -0500

    Add FontAwesome and a temporary about page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :questions
   resources :answers, only: [:create, :show]
 
+  get '/about', to: "static_pages#about", as: "about"
+
   get '/next_question', to: "question_finder#next_question", as: "next_question"
   post '/set_exam_definition', to: "question_finder#set_exam_definition", as: "set_exam_definition"
   root to: "exam_definitions#index"
