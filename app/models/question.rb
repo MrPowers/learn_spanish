399dc7e8fa106d294daf42ba5241cdc3ed92098a
MrPowers <matthewkevinpowers@gmail.com>
2015-08-08 14:07:51 -0500

    Add a validation to make sure all questions have five underscores

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,14 @@
 class Question < ActiveRecord::Base
   belongs_to :word
 
+  validate :spanish_contains_five_underscores
+
+  def spanish_contains_five_underscores
+    unless spanish.include?("_____")
+      errors.add(:spanish, "Spanish question must contain five underscores")
+    end
+  end
+
   def spanish_sentence
     spanish.gsub("_____", answer)
   end
