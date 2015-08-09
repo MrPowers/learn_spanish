85f3b5920db008dd1af39541d752eb62b917bfb9
MrPowers <matthewkevinpowers@gmail.com>
2015-08-09 08:24:12 -0500

    Update the Question#show page to make it more user friendly

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,12 @@
 module QuestionsHelper
+
+  def user_answer_text_field(f)
+    form = "<div class=user-answer>#{f.text_field(:user_answer, autofocus: true, class: "blueify", size: text_field_size, :autocomplete => :off)}<br><span class=answer-hint>#{@question.hint}</span></div>"
+    @question.spanish.gsub("_____", form)
+  end
+
+  def text_field_size
+    @question.answer.length + rand(1..5)
+  end
+
 end
