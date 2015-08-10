1ab2d974cf781d8f330bdff417f83d8d55e83e16
MrPowers <matthewkevinpowers@gmail.com>
2015-08-10 18:56:54 -0500

    Fixed a bug when the word count was zero for a given exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,6 +2,8 @@ class Exam < ActiveRecord::Base
   belongs_to :exam_definition
 
   def percent_complete
+    word_count = exam_definition.words.count
+    return 0 if word_count.zero?
     r = ((skill_level.to_f / exam_definition.words.count) * 100).round(0)
     return 100 if r > 100
     r
