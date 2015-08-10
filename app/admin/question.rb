ce8f868ab13da699e1f789b68e2977063354e1b1
MrPowers <matthewkevinpowers@gmail.com>
2015-08-10 00:54:58 -0500

    Redirect to the index page after a question is created

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -26,4 +26,18 @@ ActiveAdmin.register Question do
     actions
   end
 
+  controller do
+    def create
+      super do |format|
+        redirect_to admin_questions_path and return if resource.valid?
+      end
+    end
+
+    def update
+      super do |format|
+        redirect_to admin_questions_path and return if resource.valid?
+      end
+    end
+  end
+
 end
